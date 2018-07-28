7a900f52fe088b282aaed2669d826dc01e3315cc
SSep <ssep4u@gmail.com>
2018-07-28 17:46:08 +0900

    first commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/SSep/Documents/_funny/askcompany.kr/TourBlog/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
