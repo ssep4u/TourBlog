fea1c3baae17277f49e4ba7d9a95ec5fb49843be
SSep <ssep4u@gmail.com>
2018-07-29 18:24:35 +0900

    클래스 기반 뷰(CBV)의 4가지 패턴

--- askdjango/dojo/views.py
+++ askdjango/dojo/views.py
@@ -1,5 +1,42 @@
+import os
+from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
-def mysum(request, x, y=0, z=0):
-    return HttpResponse(int(x)+int(y)+int(z))
+
+def mysum(request, numbers):
+    numbers.split('/')
+    result = sum(map(lambda s: int(s or 0), numbers.split('/')))
+    return HttpResponse(result)
+
+
+def post_list1(request):
+    name = '공유'
+    return HttpResponse('''
+    <h1>AskDjango</h1>
+    <p>{name}</p>
+    <p>여러분의 파이썬/장고 페이스메이커가 되어드리겠습니다.</p>
+    '''.format(name=name))
+
+
+def post_list2(request):
+    name = '공유'
+    return render(request, 'dojo/post_list.html', {'name': name})
+
+
+def post_list3(request):
+    name = '공유'
+    return JsonResponse({
+        'message': '안녕 파이썬/장고',
+        'items': ['파이썬', '장고', 'Celery', 'Azure', 'AWS'],
+    }, json_dumps_params={'ensure_ascii': False})
+
+
+def excel_download(request):
+    filepath = os.path.join(settings.BASE_DIR, 'dojo/2016학급비.xlsx')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(
+            filename)
+        return response
---
+++ askdjango/dojo/views_cbv.py
@@ -0,0 +1,39 @@
+from django.views.generic import View, TemplateView
+from django.http import HttpResponse
+
+class PostListView1(View):
+    def get(self, request):
+        name = '공유'
+        return HttpResponse('''
+        <h1>AskDjango</h1>
+        <p>{name}</p>
+        <p>여러분의 파이썬/장고 페이스메이커가 되어드리겠습니다.</p>
+        '''.format(name=name))
+
+    def get_templates_string(self):
+        return '''
+        <h1>AskDjango</h1>
+        <p>{name}</p>
+        <p>엶시히 하겠습니다.</p>
+        '''.format(name=name)
+
+post_list1= PostListView1.as_view()
+
+
+class PostList2(TemplateView):
+    template_name = 'dojo/post_list.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['name']='공유'
+        return context
+
+post_list2 = PostList2.as_view()
+
+
+class PostList3(View):
+    pass
+
+
+class ExcelDownload(View):
+    pass
