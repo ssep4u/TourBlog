dfae42f72981c75b66ed3b61432ac77315d0ec09
SSep <ssep4u@gmail.com>
2018-07-31 00:36:55 +0900

    add validator in blog.models.Post.lnglat

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -1,4 +1,11 @@
+import re
 from django.db import models
+from django.forms import ValidationError
+
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 
 class Post(models.Model):
@@ -10,5 +17,9 @@ class Post(models.Model):
                                  ('제목3', '제목3 레이블'),
                              ))  # 길이 제한이 있는 문자열
     content = models.TextField()  # 길이 제한이 없는 문자열
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(
+        max_length=50, blank=True, help_text='경도, 위도 포맷으로 입력',
+        validators=[lnglat_validator])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
