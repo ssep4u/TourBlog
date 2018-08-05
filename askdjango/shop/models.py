55bd29824e83ba1bbe460ceef1024c5ad04b0bb7
SSep <ssep4u@gmail.com>
2018-08-05 13:24:26 +0900

    .

--- askdjango/shop/models.py
+++ askdjango/shop/models.py
@@ -3,8 +3,7 @@ from django.conf import settings
 
 
 class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             related_name='shop_post_set')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='shop_post_set')
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
